350cc4588aaa40ce156ff91c5a9ba05c5ef6d925
Anest <efremenko.ira.rzn@gmail.com>
2024-07-16 19:28:34 +0300

    Selenide *

--- src/test/java/ru/netology/selenide/BankCardDeliveryTest.java
+++ src/test/java/ru/netology/selenide/BankCardDeliveryTest.java
@@ -8,7 +8,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.open;
 
 
